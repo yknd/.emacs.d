49837ef2ddbea7423a49dc6891550910cdef9074
yknd <the.yknd@gmail.com>
2012-04-03 13:10:36 +0900

    Javascriptモードの修正 rcodetoolsの削除

--- init.el
+++ init.el
@@ -196,19 +196,16 @@
 ;; encoding comment
 (defun ruby-mode-set-encoding () ())
 
-;; xmpfilter
-(require 'rcodetools)
-
-;; javascript (js2-mode & espresso-mode)
-(autoload 'js2-mode "js2" nil t)
+;; javascript
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook
-          #'(lambda ()
-              (require 'js)
-              (setq js-indent-level 2
-                    js-expr-indent-offset 2
-                    indent-tabs-mode nil)
-              (set (make-local-variable 'indent-line-function) 'js-indent-line)))
+          '(lambda ()
+             (require 'js)
+             (setq js-indent-level 2
+                   js-expr-indent-offset 2
+                   indent-tabs-mode nil)
+             (set (make-local-variable 'indent-line-function) 'js-indent-line)))
 
 ;; CSS
 (defun brace-ret-brace ()
