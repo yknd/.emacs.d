2e5ecb8a65aa4eb2b50de45fba60cfebf3c5c45d
yknd <the.yknd+github@gmail.com>
2016-01-26 00:19:59 +0900

    パッケージをアップデート

---
+++ elpa/magit-popup-20160119.1409/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (22182 15446 403038 523000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
