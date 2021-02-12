ba804c46cff9c523a62faf20c8a4febdac6ff2dd
yknd <the.yknd+github@gmail.com>
2021-02-13 01:12:39 +0900

    不要になった設定を削除

--- init.el
+++ init.el
@@ -1,23 +1,22 @@
-;; ELPA
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 ;; system-type
-(setq os-type-mac (eq system-type 'darwin)
-      os-type-linux (eq system-type 'gnu/linux))
+(setq os-type-mac (eq system-type 'darwin))
 
 ;; start up
-(when os-type-linux
-  (setq initial-frame-alist
-        (append (list
-                 '(width . 168)
-                 '(height . 58)
-                 '(top . 36)
-                 '(left . 128)
-                 )
-                initial-frame-alist)))
-(when os-type-mac  ;; MacBookPro
+(setq default-frame-alist initial-frame-alist)
+(setq inhibit-startup-message t)
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(setq make-backup-files nil)
+(windmove-default-keybindings)
+(setq windmove-wrap-around t)
+(setq-default indent-tabs-mode nil)
+(setq visible-bell t)
+(server-start)
+(when os-type-mac
   (setq initial-frame-alist
         (append (list
                  '(width . 172)
@@ -25,73 +24,18 @@
                  '(top . 36)
                  '(left . 18)
                  )
-                initial-frame-alist)))
-(setq default-frame-alist initial-frame-alist)
-(setq inhibit-startup-message t)
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode nil)
-(setq make-backup-files nil)
-(windmove-default-keybindings)
-(setq windmove-wrap-around t)
-(setq-default indent-tabs-mode nil)
-(setq Man-switches "-Lja")
-(server-start)
-
-(when os-type-mac
+                initial-frame-alist))
   (setq ns-command-modifier (quote meta))  ;; command -> Alt
-  ;(setq ns-alternate-modifier (quote super))
   (setq exec-path (cons "/usr/local/bin" exec-path))
-  (setenv "PATH"
-          (concat '"/usr/local/bin:" (getenv "PATH")))
-  (setenv "LANG" "ja_JP.UTF-8"))
+  (setenv "PATH" (concat '"/usr/local/bin:" (getenv "PATH")))
+  (setenv "LANG" "ja_JP.UTF-8")
+  (set-default-font "Cica-18")
+  (set-face-font 'variable-pitch "Cica-18"))
 
-;; Buffer name
+;; buffer
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-
-;; buffer-menu
 (global-set-key "\C-x\C-b" 'buffer-menu)
-
-; line number
-;; (global-linum-mode t)
-(global-set-key [f9] 'linum-mode)
-
-; Insert current time
-(defun my-gen-dtime (form) (insert (format-time-string form)))
-(defun my-get-dtime () (interactive) (my-gen-dtime "%Y/%m/%d %H:%M:%S"))
-(global-set-key [f6] 'my-get-dtime)
-
-;; swap screen
-(defun swap-screen()
-  "Swap two screen,leaving cursor at current window."
-  (interactive)
-  (let ((thiswin (selected-window))
-        (nextbuf (window-buffer (next-window))))
-    (set-window-buffer (next-window) (window-buffer))
-    (set-window-buffer thiswin nextbuf)))
-(defun swap-screen-with-cursor()
-  "Swap two screen,with cursor in same buffer."
-  (interactive)
-  (let ((thiswin (selected-window))
-        (thisbuf (window-buffer)))
-    (other-window 1)
-    (set-window-buffer thiswin (window-buffer))
-    (set-window-buffer (selected-window) thisbuf)))
-(global-set-key [f2] 'swap-screen)
-(global-set-key [S-f2] 'swap-screen-with-cursor)
-
-;; font
-(when os-type-linux
-  (set-default-font "ricty-12")
-  (set-face-font 'variable-pitch "ricty-12"))
-(when os-type-mac
-  (set-default-font "ricty-16")
-  (set-face-font 'variable-pitch "ricty-16"))
-(set-fontset-font (frame-parameter nil 'font)
-                  'japanese-jisx0208
-                  '("ricty". "unicode-bmp")
-)
 
 ;; character code
 (set-terminal-coding-system 'utf-8)
@@ -106,25 +50,10 @@
 (require 'whitespace)
 (setq whitespace-line-column 80)
 (setq whitespace-style '(face lines-tail))
-; (add-hook 'prog-mode-hook 'whitespace-mode)
 (global-whitespace-mode +1)
 
-; mozc
-(when os-type-linux
-  (require 'mozc)
-  (setq default-input-method "japanese-mozc"))
-
 ; theme
-(load-theme 'misterioso t)
-(setq cursor-type 'hollow)
-(set-cursor-color "#4277f4")
-
-;; shell-mode
-(exec-path-from-shell-initialize)
-(global-set-key [f8] 'eshell)
-(autoload 'ansi-color-for-comint-mode-on "ansi-color"
-          "Set `ansi-color-for-comint-mode' to t." t)
-(add-hook 'eshell-load-hook 'ansi-color-for-comint-mode-on)
+(load-theme 'wombat t)
 
 ;; wdired
 (require 'wdired)
@@ -146,10 +75,6 @@
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
 (setq emmet-move-cursor-between-quotes t)
 
-;; rbenv
-(setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
-(setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
-
 ;; ruby
 (autoload 'ruby-mode "ruby-mode"
   "Mode for editing ruby source files" t)
@@ -161,29 +86,17 @@
       (append '((".rake$" . ruby-mode)) auto-mode-alist))
 (setq auto-mode-alist
       (append '(("Gemfile" . ruby-mode)) auto-mode-alist))
-(setq auto-mode-alist
-      (append '(("Vagrantfile" . ruby-mode)) auto-mode-alist))
 (setq interpreter-mode-alist
       (append '(("ruby" . ruby-mode)) interpreter-mode-alist))
 (autoload 'inf-ruby-minor-mode "inf-ruby" "Run an inferior Ruby process" t)
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
-
-;; pry
-(setq inf-ruby-default-implementation "pry")
-(setq inf-ruby-eval-binding "Pry.toplevel_binding")
-(add-hook 'inf-ruby-mode-hook 'ansi-color-for-comint-mode-on)
-
-;;ruby-electric
 (require 'ruby-electric)
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (ruby-electric-mode t)))
-
-;; flymakeRuby
 (require 'flymake)
 (set-face-background 'flymake-errline "red4")
 (set-face-background 'flymake-warnline "dark slate blue")
-;; Invoke ruby with '-c' to get syntax checking
 (defun flymake-ruby-init ()
  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
                       'flymake-create-temp-inplace))
@@ -196,32 +109,28 @@
 (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
 (add-hook 'ruby-mode-hook
          '(lambda ()
-     ;; Don't want flymake mode for ruby regions in rhtml files and also on read only files
 	     (if (and (not (null buffer-file-name)) (file-writable-p buffer-file-name))
 		 (flymake-mode))
 	     ))
-
-;; encoding comment
 (defun ruby-mode-set-encoding () ())
 
 ;; elixir
-(require 'elixir-mode)
-(add-to-list 'auto-mode-alist '("\\.elixir2\\'" . elixir-mode))
+(unless (package-installed-p 'elixir-mode)
+  (package-install 'elixir-mode))
 
 ;; javascript
 (setq js2-mirror-mode t)
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-
 (add-hook 'js2-mode-hook
           #'(lambda ()
              (require 'js)
              (setq js-indent-level 2
                    js-expr-indent-offset 2
                    indent-tabs-mode nil)
-             (set (make-local-variable 'indent-line-function) 'js-indent-line))) 
+             (set (make-local-variable 'indent-line-function) 'js-indent-line)))
 
-;; CSS
+;; css
 (defun brace-ret-brace ()
   (interactive)
   (insert "{") (newline-and-indent)
@@ -236,8 +145,6 @@
             (setq css-indent-offset 2)
             (define-key css-mode-map "{" 'brace-ret-brace)
             ))
-
-;; Sass
 (autoload 'scss-mode "scss-mode")
 (setq scss-compile-at-save nil)
 (add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
@@ -250,22 +157,4 @@
              (setq c-basic-offset 4)
              ))
 
-;; Scheme
-(setq quack-default-program "gosh")
-(require 'quack)
-
-;; CoffeeScript
-(autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
-(defun coffee-custom ()
-  "coffee-mode-hook"
-  (and (set (make-local-variable 'tab-width) 2)
-       (set (make-local-variable 'coffee-tab-width) 2))
-  )
-(add-hook 'coffee-mode-hook
-          '(lambda() (coffee-custom)))
-
-;; for Mac
 (cd "~/")
