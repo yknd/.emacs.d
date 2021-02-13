ed0b8b93c8bd649bd40d6e71e4a2d7d1762f014e
yknd <the.yknd+github@gmail.com>
2021-02-13 20:18:58 +0900

    パッケージをアップデート

---
+++ elpa/ruby-electric-20200328.1528/ruby-electric-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/rem/.emacs.d/elpa/ruby-electric-20200328.1528/ruby-electric.el  -*- no-byte-compile: t -*-
+(define-package "ruby-electric" "20200328.1528" "Minor mode for electrically editing ruby code" 'nil :commit "f2323cd9b5df3b34aa9810ba8109502824925d23" :maintainer '("Akinori MUSHA" . "knu@iDaemons.org") :keywords '("languages" "ruby") :url "https://github.com/ruby/elisp-ruby-electric")
---
+++ elpa/vterm-20210209.356/vterm-pkg.el
@@ -0,0 +1,12 @@
+(define-package "vterm" "20210209.356" "Fully-featured terminal emulator"
+  '((emacs "25.1"))
+  :commit "a3fadd28370aa43f03d4f7b197be8fa074f311f5" :authors
+  '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
+  :maintainer
+  '("Lukas Fürmetz" . "fuermetz@mailbox.org")
+  :keywords
+  '("terminals")
+  :url "https://github.com/akermu/emacs-libvterm")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
