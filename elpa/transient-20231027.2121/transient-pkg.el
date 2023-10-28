b29ecb426efc203a1a126ec3f2d982eea73cf603
rem <rem@DESKTOP-K5J1D1O.localdomain>
2023-10-28 10:43:19 +0900

    パッケージアップデート

---
+++ elpa/transient-20231027.2121/transient-pkg.el
@@ -0,0 +1,16 @@
+(define-package "transient" "20231027.2121" "Transient commands"
+  '((emacs "26.1")
+    (compat "29.1.4.1")
+    (seq "2.24"))
+  :commit "a8829875b25c0dc8cbd7163b6617c436365897b5" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainers
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("extensions")
+  :url "https://github.com/magit/transient")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- transient/history.el
+++ transient/history.el
@@ -1,1 +1,3 @@
-nil
+((magit-commit nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate")))
